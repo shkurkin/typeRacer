824544ed7ee6e8efda99d2b399b43e467388090d
Eli Shkurkin <eshkurkin@gmail.com>
2014-03-25 18:47:53 -0700

    Initial styling, changed names to race terms
    GameRoom is now Track
    Board is now Lane

---
+++ public/javascripts/track.js
@@ -0,0 +1,46 @@
+$(function(){
+  Track.init();
+})
+
+var Track = (function(){
+  function bindEvents() {
+    APP.socket.on('updateClients', updateTrack)
+  }
+
+  function updateTrack(data) {
+    updateClientList(data);
+    updateLanes(data);
+  }
+
+  function updateClientList(data) {
+    var clientList = ''
+    for(var i=0; i<data.length; i++) {
+      clientList += '<p class="user">' + data[i] + '</p>'
+    }
+    $('#currentUsers').html(clientList);
+  }
+
+  function updateLanes(data) {
+    for (var i=0; i<data.length; i++) {
+      if($("." + data[i]).length === 0 && data[i] != APP.user) {
+        appendLane(data[i]);
+      }
+    }
+  }
+
+  function appendLane(username) {
+    var newLane = $('#template').clone();
+    newLane.find('.username').html(username);
+    newLane.find('.text').attr('id', username);
+    newLane.show();
+    $('#otherLanes').append(newLane);
+  }
+
+  function _init() {
+    bindEvents();
+  }
+
+  return {
+    init: _init
+  }
+}());
